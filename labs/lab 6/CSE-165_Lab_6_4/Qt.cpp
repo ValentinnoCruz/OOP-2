c8defade76ea7896b684e79e091ac7b9964411ae
Valentinno Cruz <valentinnocruz@gmail.com>
2022-03-10 11:11:55 -0800

    lab 6 4

---
+++ labs/lab 6/CSE-165_Lab_6_4/Qt.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Qt.cpp
+ * Author: Didis
+ *
+ * Created on March 8, 2022, 8:02 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
