968e0b1b8d4775ce38d523834f1b88492e08b6e7
valentinn cruz <valentinnocruz@gmail.com>
2022-03-15 11:18:15 -0800

    Q1

---
+++ labs/lab 7/CSE-165_lab7_1/build/Debug/Cygwin-Windows/refs.o.d
@@ -0,0 +1,2 @@
+build/Debug/Cygwin-Windows/refs.o: refs.cpp refs.h
+refs.h:
---
+++ labs/lab 7/CSE-165_lab7_1/refs.h
@@ -0,0 +1,16 @@
+#ifndef REFS_H
+#define REFS_H
+
+using namespace std;
+
+
+void triple(int &n)
+{
+
+n = n*3;
+
+}
+
+#endif
+
+
